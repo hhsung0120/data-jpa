3d2a37d055e893c30a4f19629cae34f5eae95c45
hshan <hhsung0120@naver.com>
2023-04-06 22:50:42 +0900

    2023-04-06  * 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -27,9 +30,28 @@ public class MemberController {
         return member.getUsername();
     }
 
-    @PostConstruct
-    public void init() {
-        memberRepository.save(new Member("userA"));
+    //page, size, sort 변수로 자동 매핑
+    //http://localhost:8080/members?page=0&size=1&sort=id,desc&sort=username,desc
+    //디폴트 값 설정은 yml
+    //여기에만 특별하게 사용하고 싶다면
+    //@PageableDefault(size = 10, page = 1) Pageable pageable
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
     }
 
+    @GetMapping("/members2")
+    public Page<MemberDto> listDto(Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+    }
+
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
 }
